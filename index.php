15041db9827c8e15e825e3a2e9d309a2759f61b5
tiantaljard <1511762@rgu.ac.uk>
2017-02-28 15:42:06 +0000

    tt

--- index.php
+++ index.php
@@ -6,6 +6,11 @@
         <p>
             <?php
             echo "Hello World";
+            echo "Hello, world!";  echo "Hello," . " " . "world" . "!";
+            echo 5 * 7;
+            $myname = "Frodo Baggins";
+            $myage = 111;
+            echo "My name is" . $myname . "and I am " . $myage;
             ?>
         </p>
         </body>
