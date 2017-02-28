3fc9b2cb42b355c944a491477c247d5ef4303801
tiantaljard <1511762@rgu.ac.uk>
2017-02-28 16:16:24 +0000

    tt t

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     echo "Hello, world!";  echo "Hello," . " " . "world" . "!";
     echo 5 * 7;
     $myname = "Frodo Baggins";
-    $myage = 17;
+    $myage = 14;
     echo "My name is" . $myname . "and I am TWAT" . $myage;
     ?>
     <?php     echo "I get printed!";
